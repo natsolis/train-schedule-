8f03b81907fbe7e78f111381dac887832b736cde
Nathalie Solis-Maravilla <nathaliesolis-maravilla@Nathalies-MacBook-Pro.local>
2019-06-24 12:51:13 -0600

    front-end done

---
+++ javascript/train.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDJy3HphxtTCgCIiITgzZQmbVdjAcvU0HM",
+    authDomain: "train-schedule-5caef.firebaseapp.com",
+    databaseURL: "https://train-schedule-5caef.firebaseio.com",
+    projectId: "train-schedule-5caef",
+    storageBucket: "train-schedule-5caef.appspot.com",
+    messagingSenderId: "773603576695",
+    appId: "1:773603576695:web:0d2c12b651013d4c"
+  };
+  
+  firebase.initializeApp(firebaseConfig);
+
+  var database = firebase.database();
